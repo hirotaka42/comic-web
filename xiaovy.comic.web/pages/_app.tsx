e5d5114d823190920462bd7da0def27322ff383e
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-06 20:43:22 +0900

    update: globals.cssをstyled-componentsを用いて記載

--- xiaovy.comic.web/pages/_app.tsx
+++ xiaovy.comic.web/pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
 import type { AppProps } from 'next/app'
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  
+  h1,
+  h2 {
+    font-weight: bold;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    html {
+      color-scheme: dark;
+    }
+    body {
+      color: white;
+      background: black;
+    }
+  }
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+  
 }
 
 export default MyApp
