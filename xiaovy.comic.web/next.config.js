400698951157bed6725e7b8d7e507d2e93b97e07
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-06 23:16:37 +0900

    update: Styled Componentsで設定したCSSが正しく反映されるように修正

--- xiaovy.comic.web/next.config.js
+++ xiaovy.comic.web/next.config.js
@@ -9,6 +9,31 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // exportPathMapを設定して静的ページのパスを明示的に指定する
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // 他のページのパスを追加する
+    };
+  },
+  // CSS最適化の設定
+  webpack: (config, { dev, isServer, webpack }) => {
+    if (!dev && !isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
