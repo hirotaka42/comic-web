5da84692f39574a44a2227f2ec3962dd6d548668
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-07 14:01:32 +0900

    test: cssが表示されないことに対するテスト

--- xiaovy.comic.web/pages/_app.tsx
+++ xiaovy.comic.web/pages/_app.tsx
@@ -1,46 +1,47 @@
+import '../styles/globals.css';
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 import type { AppProps } from 'next/app';
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
+// const GlobalStyle = createGlobalStyle`
+//   ${reset}
+//   html,
+//   body {
+//     padding: 0;
+//     margin: 0;
+//     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+//       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+//   }
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+//   a {
+//     color: inherit;
+//     text-decoration: none;
+//   }
 
-  * {
-    box-sizing: border-box;
-  }
-  
-  h1,
-  h2 {
-    font-weight: bold;
-  }
+//   * {
+//     box-sizing: border-box;
+//   }
 
-  @media (prefers-color-scheme: dark) {
-    html {
-      color-scheme: dark;
-    }
-    body {
-      color: white;
-      background: black;
-    }
-  }
-`;
+//   h1,
+//   h2 {
+//     font-weight: bold;
+//   }
+
+//   @media (prefers-color-scheme: dark) {
+//     html {
+//       color-scheme: dark;
+//     }
+//     body {
+//       color: white;
+//       background: black;
+//     }
+//   }
+// `;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
       <Component {...pageProps} />
     </>
   );
