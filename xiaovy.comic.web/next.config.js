95ef91651f1ea20c2c61c1244f026ae85a7272bb
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-07 20:08:39 +0900

    rm: MUIに移行するために競合するstyled-componentsに関連するものを削除

--- xiaovy.comic.web/next.config.js
+++ xiaovy.comic.web/next.config.js
@@ -2,37 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
   // next exportを使えるように画像最適化APIを無効化
   images: {
     unoptimized: true,
-  },
-  // exportPathMapを設定して静的ページのパスを明示的に指定する
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      // 他のページのパスを追加する
-    };
-  },
-  // CSS最適化の設定
-  webpack: (config, { dev, isServer, webpack }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks.cacheGroups = {
-        ...config.optimization.splitChunks.cacheGroups,
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true,
-        },
-      };
-    }
-    return config;
   },
 }
 
