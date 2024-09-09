4f4bc91412e63172c16ed0c601fdacb8cbe6fbb4
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-09 20:35:41 +0900

    feat: デプロイ後も画像パスがずれない様に修正、修正に伴いエイリアスを使用できる様に設定の追加

--- xiaovy.comic.web/src/components/Templates/ComicHome.tsx
+++ xiaovy.comic.web/src/components/Templates/ComicHome.tsx
@@ -1,6 +1,7 @@
 import { Box, ButtonGroup, Button } from '@mui/material';
 import { ComicList } from '../Organisms/ComicList';
 import Image from 'next/image';
+import PRimage00 from '@assets/etc/sample00.webp';
 
 export const ComicHome: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ export const ComicHome: React.FC = () => {
       {/* 上部の大きな画像 */}
       <Box sx={{ position: 'relative', height: '400px', width: '100%' }}>
         <Image
-          src="https://github-production-user-asset-6210df.s3.amazonaws.com/79750434/365408047-9cbaba22-9af2-4732-bfd0-0c0d8350980e.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240908%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240908T012121Z&X-Amz-Expires=300&X-Amz-Signature=40a93f001a845dfb9828058268763143e14a64cd32d41313f8acb3f5cf2b01ed&X-Amz-SignedHeaders=host&actor_id=79750434&key_id=0&repo_id=853287068" // パスを直接指定
+          src={PRimage00.src}
           alt="MainPR image"
           layout="fill" // 親要素に対して幅と高さを100%にする
           objectFit="cover" // アスペクト比を維持しつつ、コンテナ全体をカバー
