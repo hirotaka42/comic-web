527a5df53ac9d576fe58ddd5ea37432a165b10bb
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2024-09-09 21:33:44 +0900

    test: 画像表示のためのテスト

--- xiaovy.comic.web/next.config.js
+++ xiaovy.comic.web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = !isProd ? '/comic-web' : ''
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
